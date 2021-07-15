81b66a2b04ee53d583afd69bb007d05d33e7d03c
Konstantin Cherepanov <k.s.cherepanov@gmail.com>
2021-07-15 15:57:10 +0300

    practicalTask_2_1_2 solution

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloWorldBean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorldBean12 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorldBean1.getMessage());
+    }
+}
